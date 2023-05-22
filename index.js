6952a96f9b675b1882965a7f63b258052ca5d008
rylanp <91034086+rylanp@users.noreply.github.com>
2023-05-21 22:34:30 -0500

    j

---
+++ index.js
@@ -0,0 +1,78 @@
+
+
+var fields = {}
+var timer = 0
+document.addEventListener("DOMContentLoaded", function(){
+    fields.name = document.getElementById("name");
+    fields.email = document.getElementById("email");
+    fields.message = document.getElementById("message");
+})
+
+function isNotEmpty(value) {
+    if (value == null || typeof value == 'undefined' ) return false;
+    return (value.length > 0);
+   }
+function isEmail(email) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(email).toLowerCase());
+}
+
+function fieldValidation(field, validationFunction) {
+    if (field == null) return false;
+
+    let isFieldValid = validationFunction(field.value)
+    if (!isFieldValid) {
+        field.className = 'placeholderRed';
+    } else {
+        field.className = '';
+    }
+
+    return isFieldValid;
+}
+
+function isValid() {
+    var valid = true;
+    
+    valid &= fieldValidation(fields.name, isNotEmpty);
+    valid &= fieldValidation(fields.message, isNotEmpty);
+    valid &= fieldValidation(fields.email, isEmail);
+   
+    return valid;
+}
+class User {
+    constructor(name, email, message) {
+        this.name = name;
+        this.email = email;
+        this.message = message;
+    }
+}
+function sendContact(){
+    if (isValid()) {
+        let usr = new User(fields.name.value,  fields.email.value, fields.message.value);
+        sendEmail(usr)
+    }else{
+        alert('Error submitting feedback')
+    }
+}
+function sendEmail(user){
+    // uses smtp.js
+    if (timer > 0){
+        alert("Wait before submitting another email")
+        return
+    }
+    Email.send({
+        SecureToken : "c0105397-ade2-4353-96cb-d58f53197ef4",
+        To : "discoverposeycounty@gmail.com",
+        From : "discoverposeycounty@rylanpaul.com",
+        Subject : "discoverposeycounty.rylanpaul.com Form Submission",
+        Body : "Name: " + user.name + "<br>Email: " + user.email + "<br><br>Message:<br>" + user.message
+    }).then(
+      message => alert(message + "\n" + user.name + ', thanks for the feedback!')
+    );
+    timer = 1
+    setTimeout(addTimer, 180000)
+}
+
+function addTimer(){
+   timer = 0
+}
